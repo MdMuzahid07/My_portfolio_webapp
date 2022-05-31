20e16fde8a5659a084041b5f516a668cc8f37b91
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-06-01 04:33:01 +0600

    navbar design complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Header/Navbar';
+import Home from './pages/Home/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='max-w-screen-md mx-auto'>
+        <Navbar />
+        <Home />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='md:max-w-lg mx-auto px-10'>
+            <h1>this is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
