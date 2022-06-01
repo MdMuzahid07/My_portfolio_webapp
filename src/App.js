8d927c0c72bb758453218065e9680fde56489cf8
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-06-01 08:45:49 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 import Navbar from './components/Header/Navbar';
 import Home from './pages/Home/Home';
 import Posts from './pages/Posts/Posts';
@@ -13,14 +14,15 @@ const App = () => {
     <div className='bg-base-100' data-theme={dark ? "dark" : "light"}>
       <div className='max-w-screen-md mx-auto'>
         <Navbar dark={dark} setDark={setDark} />
-          <div>
-            <img className='max-w-48 max-h-48 mx-auto ' src="https://api.lorem.space/image/face?hash=92048" alt="" />
-          </div>
+        <div>
+          <img className='max-w-48 max-h-48 mx-auto ' src="https://api.lorem.space/image/face?hash=92048" alt="" />
+        </div>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/works' element={<Works />} />
           <Route path='/posts' element={<Posts />} />
         </Routes>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className='text-accent text-center mb-5'>Copyright © 2022 - All right reserved</p>
+        </footer>
+    );
+};
+
+export default Footer;
