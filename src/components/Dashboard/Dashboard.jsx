312f00860f472d23c7869262b567e8b9b2e0626c
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-06-01 23:28:21 +0600

    dashboard updated

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,23 +3,23 @@ import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <div className='max-w-xl  mx-auto my-5'>
-            <div class="drawer drawer-mobile bg-parimary">
+        <div className='max-w-xl  mx-auto my-5 px-5'>
+            <div class="drawer rounded-xl drawer-mobile bg-parimary">
                 <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-                <div class="drawer-content flex flex-col items-center justify-center">
+                <div class="drawer-content  rounded-xl shadow-xl bg-accent text-white flex flex-col items-center justify-center">
                     {/* <!-- Page content here --> */}
                     <Outlet />
 
                 </div>
-                <div class="drawer-side rounded-xl shadow-xl">
+                <div class="drawer-side">
                     <label for="my-drawer-2" class="drawer-overlay"></label>
-                    <ul class="menu p-4 overflow-y-auto w-80 text-base-content">
+                    <ul class="menu p-4 bg-white rounded-xl overflow-y-auto w-xl text-base-content">
                         {/* <!-- Sidebar content here --> */}
                         <li>
-                            <Link className='btn btn-outline btn-wide mb-2' to='postblog'>Post Blogs</Link>
+                            <Link className='btn btn-outline mb-2' to='postblog'>Post Blogs</Link>
                         </li>
                         <li>
-                            <Link className='btn btn-outline btn-wide mb-2' to='postwork'>Post Works</Link>
+                            <Link className='btn btn-outline mb-2' to='postwork'>Post Works</Link>
                         </li>
                     </ul>
 
