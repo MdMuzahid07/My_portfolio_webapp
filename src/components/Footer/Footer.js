44eb51ba82999f5b371e46499a9c1779a9254021
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-06-01 08:52:05 +0600

    navbar updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p className='text-accent text-center mb-5'>Copyright © 2022 - All right reserved</p>
+            <p className='text-accent text-center pb-7'>Copyright © 2022 - All right reserved</p>
         </footer>
     );
 };
